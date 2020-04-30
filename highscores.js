4c8cb75189e1b7d4c57f7f253abaeb5193ee7b4d
Pravin Sisowath <pravinsisowath@gmail.com>
2020-04-29 19:26:25 -0700

    fixed storage for high scores

--- highscores.js
+++ highscores.js
@@ -4,7 +4,7 @@ let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 highScoresList.innerHTML =
     highScores
         .map(score => {
-            return <li class ="high-score">${score.name} - ${score.score}</li>
+            return `<li class ="high-score">${score.name} - ${score.score}</li>`
         })
         .join('')
 
